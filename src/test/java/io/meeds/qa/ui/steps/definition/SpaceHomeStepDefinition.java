40668f72e33a235bc5332070edf2165bbe0fbefe
anisbenabdallah <abenabdallah@exoplatform.com>
2022-11-21 17:33:13 +0100

    PinActivity_US04: Pinned activities filter

--- src/test/java/io/meeds/qa/ui/steps/definition/SpaceHomeStepDefinition.java
+++ src/test/java/io/meeds/qa/ui/steps/definition/SpaceHomeStepDefinition.java
@@ -823,4 +823,9 @@ public class SpaceHomeStepDefinition {
   public void goToUserProfileFromLikersDrawer(String prefix) {
     spaceHomeSteps.goToUserProfileFromLikersDrawer(prefix);
   }
+
+  @When("^I select Pinned activities from the filter proposed")
+  public void selectPinnedActivity() {
+    spaceHomeSteps.selectPinnedActivity();
+  }
 }
