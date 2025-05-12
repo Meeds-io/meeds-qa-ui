f4334becd2b06b21dbe574fb52b5dfc5f518e9b6
Houssem Ben Ali <hbenali.tn@gmail.com>
2025-05-12 15:38:46 +0200

    TMP: Restore removed myApplicationDrawerElement method to fix CI
    (cherry picked from commit 7cada2744a501783e3e8140f38694699410b39da)

--- src/main/java/io/meeds/qa/ui/pages/ApplicationPage.java
+++ src/main/java/io/meeds/qa/ui/pages/ApplicationPage.java
@@ -347,6 +347,10 @@ public class ApplicationPage extends GenericPage {
                                           appTitle));
   }
 
+  private ElementFacade myApplicationDrawerElement() {
+    return findByXPathOrCSS("//*[@id='appLauncher']");
+  }
+
   private ElementFacade closeDeletePopupButtonElement() {
     return findByXPathOrCSS(".v-dialog--active .uiIconClose");
   }
